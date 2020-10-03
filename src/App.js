7579b5d9e01afabb71fa0dc253e882193db1e25d
palash117 <palash2392@gmail.com>
2020-10-03 14:47:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import logo from './LOGO_512.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.app}>
+      <div style={styles.container}>
+        <div style={styles.imageContainer} className="imageContainer">
+          <img style={styles.image} src={logo} className="image" ></img>
+        </div>
+        <div style={styles.messageContainer}>
+          <div style={styles.message}>
+            contact us at support@kodecke.com
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
+const styles = {
+  app:{
+    width:"100%",
+    borderWidth:"1px",
+    height:"100%"
+  },
+  container:{
+    display:"flex",
+    height:"100%",
+    
+    flexDirection:"column",
+    alignItems:"center",
+    justifyContent:"center"
+    
+  },
+  imageContainer:{
+    width:"200px",
+    height:"200px",
+    animation:"logo-pop"
+  },
+  image:{
+    width:"200px",
+    height:"200px",
+    
+  },
+  messageContainer:{
+    // alignSelf:"flex-start",
+    // position:"absolute",
+    alignItems:"center",
+    bottom:"20px",
+    color:"white",
+    // borderWidth:"1px",
+    // borderColor:"black"
+  },
+  message:{
+    color:"white"
+  }
+}
 export default App;
