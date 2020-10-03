b59117526fc224096a306ab4b08e1e551cb44f5a
palash117 <palash2392@gmail.com>
2020-10-03 14:50:35 +0530

    docs folder commit

---
+++ docs/precache-manifest.95161cb079a46f22e4fd117962a69cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e8d39d713abbb193f4f8c032631f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c62e78d3be6c56b73336",
+    "url": "/static/css/main.03b50610.chunk.css"
+  },
+  {
+    "revision": "498d81862df8bee17332",
+    "url": "/static/js/2.76584117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76584117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62e78d3be6c56b73336",
+    "url": "/static/js/main.cf44cec8.chunk.js"
+  },
+  {
+    "revision": "6fdfca9ac9ecc5cf24c5",
+    "url": "/static/js/runtime-main.0692c151.js"
+  },
+  {
+    "revision": "48ed16009d5c5d2679258cac53f014f9",
+    "url": "/static/media/LOGO_512.48ed1600.png"
+  }
+]);
